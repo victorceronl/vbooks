16c90580a27805390d68af3d020273a317862897
victorceronl <ceronloezavictor@gmail.com>
2025-10-19 16:41:31 -0600

    Agregando quitando modo produccion

--- v_books/v_books/settings.py
+++ v_books/v_books/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ws=c6sjqf0ubvk3x4$8l$t48_d97_!)$0xfd-(u%s005ojr3ac'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
 ALLOWED_HOSTS = ['victorceronl.pythonanywhere.com', '127.0.0.1', 'localhost']
 
