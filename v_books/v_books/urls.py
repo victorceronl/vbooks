b9b3a029e97a08d54d9daff869379318a106fcf6
victorceronl <ceronloezavictor@gmail.com>
2025-10-19 15:21:00 -0600

    Modificación para redirigir app principal

--- v_books/v_books/urls.py
+++ v_books/v_books/urls.py
@@ -18,11 +18,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView  # Import para redirección
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vbooks_01/', include('vbooks_01.urls')),
+    path('', RedirectView.as_view(url='/vbooks_01/', permanent=True)),  # Redirige la raíz
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
